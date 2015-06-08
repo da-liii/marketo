6576cede953d749a013adab2094b45b810f6c2c7
Darcy Shen <sadhen@zoho.com>
2015-06-08 20:58:33 +0800

    Add context menu for treeview

--- marknote/navpanel.cpp
+++ marknote/navpanel.cpp
@@ -1,5 +1,4 @@
 #include "navpanel.h"
-// #include "kmarknote_generalsettings.h"
 
 #include <KConfigGroup>
 #include <KSharedConfig>
@@ -11,11 +10,18 @@
 #include <QVBoxLayout>
 #include <QModelIndex>
 #include <QUrl>
+#include <QMenu>
+#include <QAction>
+#include <QContextMenuEvent>
+#include <QMessageBox>
+#include <QInputDialog>
+#include <QDebug>
 
 Navigator::Navigator(Panel* parent)
     : Panel(parent)
 {
     KConfigGroup cfg(KSharedConfig::openConfig(), "General Options");
+    m_pos = QPoint(0, 0);
     
     tmodel = new QFileSystemModel;
     tmodel->setRootPath(cfg.readEntry("NoteDir"));
@@ -34,10 +40,13 @@ Navigator::Navigator(Panel* parent)
     treeView->setIconSize(QSize(treeView->sizeHint().width(), 34));
     treeView->setHeaderHidden(true);
     treeView->setAnimated(true);
+    treeView->setContextMenuPolicy(Qt::CustomContextMenu);
     
     connect(treeView, SIGNAL(clicked(QModelIndex)),
             this, SLOT(setUrlFromIndex(QModelIndex)));
-    
+    connect(treeView, SIGNAL(customContextMenuRequested(const QPoint&)),
+            this, SLOT(showContextMenu(const QPoint&))); 
+
     vl = new QVBoxLayout(this);
     vl->addWidget(treeView);
 }
@@ -64,6 +73,47 @@ bool Navigator::urlChanged()
 
     return true;
 }
+
+void Navigator::showContextMenu(const QPoint& pos)
+{
+    m_pos = pos;
+    QMenu *contextMenu = new QMenu();
+    QAction *newDirAction = contextMenu->addAction(QString("New Directory"));
+    QAction *deleteDirAction = contextMenu->addAction(QString("Delete Directory"));
+    connect(newDirAction, SIGNAL(triggered()), this, SLOT(slotNewDir()));
+    connect(deleteDirAction, SIGNAL(triggered()), this, SLOT(slotDeleteDir()));
+    
+    if (contextMenu) {
+        contextMenu->exec(QCursor::pos());
+    }  
+    delete contextMenu;
+}
+
+void Navigator::slotNewDir()
+{
+    QModelIndex index = treeView->indexAt(m_pos);
+    QDir dir(tmodel->filePath(index));
+
+    QString folderName = QInputDialog::getText(this,
+                                                QString("Create Folder in ") + tmodel->filePath(index),
+                                                QString("Folder Name"));
+    dir.mkdir(folderName);
+}
+
+void Navigator::slotDeleteDir()
+{
+    QModelIndex index = treeView->indexAt(m_pos);
+    QUrl url(tmodel->filePath(index));
+    QDir dir(url.adjusted(QUrl::RemoveFilename).toLocalFile());
+    
+    if (!dir.rmdir(url.fileName())) {
+        QMessageBox message;
+        message.setWindowTitle("Delete folder");
+        message.setText(QString("Cannot delete nonempty folder ") + url.fileName());
+        message.exec();
+    }
+}
+
 
 Navigator::~Navigator()
 {
